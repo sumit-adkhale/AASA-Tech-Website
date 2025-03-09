a0336dff94a563421519fcdd7776749d2745f4dc
sumit-adkhale <sumit.adkhale5@gmail.com>
2025-03-09 23:46:55 +0530

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="text-center pb-10 md:pb-10">
       <div className="flex justify-between md:flex md:justify-between items-center">
-        <img className="w-[25vw]  md:w-[200px]" src="/logo.png" alt="" />
+        <img className="w-[25vw]  md:w-[200px]" src="logo.png" alt="" />
         <Button customcss="rounded-full text-lg " data="Get Started" />
       </div>
     </header>
